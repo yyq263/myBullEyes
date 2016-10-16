baf2f69f670285f1cd5ffb6118ee4f019b3bb7fb
Yiqin Yao <yy1165@nyu.edu>
2016-10-16 22:28:10 +0800

    basic frame and complete game functionality.

---
+++ BullsEye/AboutViewController.swift
@@ -0,0 +1,39 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by Yiqin Yao on 15/10/2016.
+//  Copyright © 2016 Yiqin Yao. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    @IBAction func close(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        // Do any additional setup after loading the view.
+//    }
+//
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//        // Dispose of any resources that can be recreated.
+//    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -9,10 +9,92 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var roundLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var targetLabel: UILabel! //Interface Building
+    var currentValueOfSlider: Int = 0
+    var targetValue: Int = 0
+    var score = 0
+    var round = 0
+    
+    @IBOutlet weak var slider: UISlider!
+    
+    @IBAction func sliderMoved(_ sender: UISlider) {
+        //print("Slider now is at \(sender.value)")
+        currentValueOfSlider = lroundf(sender.value)
+    }
+    
+    func startNewGame() {
+        score = 0
+        round = 0
+        startNewRound()
+    }
+    
+    @IBAction func staroverButton(_ sender: UIButton) {
+        startNewGame()
+        updateLabels()
+    }
+    
+    @IBAction func showAlert(_ sender: UIButton) {
+        
+        let difference = abs(currentValueOfSlider - targetValue)
+        var points = 100 - difference
+        
+        let title: String
+        if difference == 0 {
+            title = "Perfect!"
+            points += 100
+        } else if difference < 5 {
+            title = "You almost had it!"
+            if difference == 1 {
+                points += 50
+            }
+        } else if difference < 10 {
+            title = "Pretty good!"
+        } else {
+            title = "Not even close..."
+        }
+        
+        score += points
+        
+        let message = "Your scored \(points) points"
+        
+        let alert = UIAlertController(title: title,
+                                      message: message,
+                                      preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "OK",
+                                   style: .default) {
+                                                    action in
+                                                    self.startNewRound()
+                                                    self.updateLabels()
+                                                    }// handler: tells the alert what should happen when the button is pressed. This is the “alert dismissed” event you’ve been looking for.
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
 
+    }
+    
+    
+    func startNewRound() {
+        round += 1
+        targetValue = 1 + Int(arc4random_uniform(100))
+        currentValueOfSlider = 50
+        slider.value = Float(Double(currentValueOfSlider))
+    }
+    
+    func updateLabels() {
+        targetLabel.text = String(targetValue)
+        scoreLabel.text = String(score)
+        roundLabel.text = String(round)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        startNewGame()
+        updateLabels()
     }
 
     override func didReceiveMemoryWarning() {
